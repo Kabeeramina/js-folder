5f6b7e22fbcd55fa266e08a176897284b1e34688
Kabeeramina <ameenakabeer209@gmail.com>
2022-06-25 13:13:06 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var firstName="Amina",
+    lastName="Kabir";
+ var fullName=firstName +  " "   + lastName;
+
+
+ var height= "5 ft. 5";
+
+ var country= "Nigeria";
+
+ alert(fullName);
+
+ alert(height);
+
+ alert(country);
